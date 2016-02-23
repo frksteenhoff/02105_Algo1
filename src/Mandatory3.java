4cfd852d23facd4303546c896524ce9d1a07b87f
Henriette Steenhoff <henriette_steenhoff@outlook.dk>
2016-02-23 12:23:11 +0100

    Mandatory 3 finished

--- src/Mandatory3.java
+++ src/Mandatory3.java
@@ -1,23 +1,45 @@
-
+/*
+ * Henriette Steenhoff, s134869
+ * 02105 - Algoritmer og Datastrukturer 1
+ * Mandatory 3
+ */
 public class Mandatory3 {
 
 	public static void main(String[]args){
 		
-		int[] B = {12,4,23,11,-2,3,4,1,1};
+		// The different lists
+		// (The values are equally valid, since they are not used in the program)
+		int[] B;
+		int[] C; 
+		int[] D;  		
+		
+		//Allocation of memory
+		B = new int[800];
+		C = new int[1600];
+		D = new int[3200];
+		
+		// Run of the program
 		System.out.println("Run of ALG3");
 		ALG3(B);
+		ALG3(C);
+		ALG3(D);
 	}
 
 	private static void ALG3(int[] b) {
+		// Run time variable start 
+		final long start = System.currentTimeMillis();
+		
 		int k = 0;
-		for(int i = 0 ; i < b.length ; i++){
-			for(int j = 0 ; j < b.length ; j++){
+		for(int i = 1 ; i <= b.length ; i++){
+			for(int j = 1 ; j <= b.length ; j++){
 				k = j;
 				while(k <= b.length){
 					k = k * 3;
 				}
 			}
 		}
-		System.out.println("Ended, " + k);
+		// Run time variable end + print
+		final long end = System.currentTimeMillis();
+		System.out.println("Execution time: " + (end - start) );
 	}
 }
